549873af2ec692746257abb716fae87f46951306
alekseygenerozov <aleksey.generozov@gmail.com>
2018-11-23 15:41:55 -0700

    Worked out some kinks in the parameter fitting file.

--- stack_elems.py
+++ stack_elems.py
@@ -12,11 +12,13 @@ def cum_dist(x, xmin, xmax, p):
 elem='a'
 names=np.genfromtxt('names', dtype=str)
 
+f=open('pfit', 'w')
 for ii in range(0, 4999):
-	elem_list=np.concatenate([Table.read(name, '/{0}'.format(ii))[elem] for name in names])
+	elem_list=np.concatenate([Table.read(name.replace('.bin', '_elems.hdf5'), '/{0}'.format(ii))[elem] for name in names])
+	elem_list=elem_list[elem_list>0]
 
 	xx=np.sort(elem_list)
 	yy=(np.array(range(len(xx)))).astype(float)/float(len(xx))
-	popt,pcov=curve_fit(cum_dist, xx, yy, [1., 2., 2.])
-	print popt
-
+	popt,pcov=curve_fit(cum_dist, xx, yy, [1., 2., 1.1])
+	f.write('{0} {1} {2} {3}\n'.format(ii, popt[0], popt[1], popt[2]))
+f.close()
