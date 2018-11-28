967c232320bbddf2a343c4662513ba471c75a4e4
alekseygenerozov <aleksey.generozov@gmail.com>
2018-11-27 17:09:06 -0700

    Velocity info from sims to hdf5 file.

---
+++ sig_gen_filt.py
@@ -0,0 +1,38 @@
+import sys
+import rebound
+import numpy as np
+from astropy.table import Table
+
+name=sys.argv[1]
+num_bins=sys.argv[2]
+num_bins=int(num_bins)
+print name
+sa=rebound.SimulationArchive(name)
+ms=np.genfromtxt(name.replace('.bin', '_masses'))
+
+sigs_light=np.empty([num_bins, len(sa), 4])
+sigs_heavy=np.empty([num_bins, len(sa), 4])
+for ii,sim in enumerate(sa):
+	##Orbital smas and eccentricities
+	orbits = sim.calculate_orbits(primary=sim.particles[0])
+	odat=np.array([[oo.a, oo.e] for oo in orbits])
+	a1=odat[:,0]
+	eccs=odat[:,1]
+	##Look at just the light stars
+	filt_light=(ms<=np.median(ms))
+	# filt_heavy=(ms>1.0e-4)
+
+	vs=np.array([pp.vz for pp in (sim.particles[1:])])
+	for jj in range(num_bins):
+		##Bin particles by sma and compute the velocity in each bin -- May be good to try binning by radius as well 
+		a_in=1.+(1./num_bins)*jj
+		a_out=1.+(1./num_bins)*(jj+1.)
+
+		filt=((a1>=a_in) & (a1<a_out))
+		# if len(eccs[filt & filt_light])==0:
+		# 	tmp=np.inf
+		# 	sigs_light[jj,ii]=np.array([np.inf, np.inf, np.inf, np.inf])
+		# 	continue
+		# tmp = np.mean(eccs[filt & filt_light]**2.)**0.5/2.**0.5*(1./(np.mean([a_in, a_out])))**0.5
+		# sigs_light[jj,ii] = np.hstack([np.std(vs[filt & filt_light], axis=0), tmp])
+		Table(vs[filt & filt_light]).write(name.replace('.bin', '_vs.h5'), path='/bin/{0}/{1}'.format(ii, jj), append=True, overwrite=True)
